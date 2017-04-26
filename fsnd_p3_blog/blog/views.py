5d5dc25e48f4c04fe6f982c438ca9a31a8a075b9
jwaltzjr <jwaltzjr@hotmail.com>
2017-04-26 15:51:45 -0500

    Add MainView

--- fsnd_p3_blog/blog/views.py
+++ fsnd_p3_blog/blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView, ListView
+
+from . import models
 
 # Create your views here.
 
-def index(request):
-  return HttpResponse('testing')
+class MainView(ListView):
+    template_name = 'blog.html'
+    model = models.BlogPost
+
+    def get_context_data(self, **kwargs):
+        context = super(ListView, self).get_context_data(**kwargs)
+        # context['user'] = self.check_login()
+        return context
