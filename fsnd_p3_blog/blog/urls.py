9256bf9bad34af1ac286ebadfb0e13b6e3f94bb6
jwaltzjr <jwaltzjr@hotmail.com>
2017-04-27 15:00:43 -0500

    Add Login and Logout views

--- fsnd_p3_blog/blog/urls.py
+++ fsnd_p3_blog/blog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.MainView.as_view(), name='main'),
+    url(r'^$', views.MainPage.as_view(), name='main'),
     url(r'^user/$', views.test, name='user_redirect'),
     url(r'^user/([A-Za-z0-9]+)/$', views.test, name='user_view'),
     url(r'^newpost/$', views.test, name='newpost'),
@@ -18,6 +18,6 @@ urlpatterns = [
     url(r'^([0-9]+)/([0-9]+)/unlike/$', views.test, name='comment_unlike'),
     url(r'^signup/$', views.test, name='signup'),
     url(r'^welcome/$', views.test, name='welcome'),
-    url(r'^login/$', views.test, name='login'),
-    url(r'^logout/$', views.test, name='logout')
+    url(r'^login/$', views.LoginPage.as_view(), name='login'),
+    url(r'^logout/$', views.LogoutPage.as_view(), name='logout')
 ]
--- fsnd_p3_blog/blog/views.py
+++ fsnd_p3_blog/blog/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView
+from django.contrib.auth import views as auth_views
 
 from . import models
 
 # Create your views here.
 
-class MainView(ListView):
+class MainPage(ListView):
     template_name = 'blog.html'
     model = models.BlogPost
+
+class LoginPage(auth_views.LoginView):
+    template_name = 'login.html'
+
+class LogoutPage(auth_views.LogoutView):
+    template_name = 'logout.html'
 
 def test(request):
     return HttpResponse('testing, testing...')
