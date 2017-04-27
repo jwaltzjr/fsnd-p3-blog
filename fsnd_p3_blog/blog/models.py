b42cd3cfdc254d3afcb2121b34c3aacb0f00fd7e
jwaltzjr <jwaltzjr@hotmail.com>
2017-04-27 11:38:18 -0500

    Remove duplicate import

--- fsnd_p3_blog/blog/models.py
+++ fsnd_p3_blog/blog/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib import contenttypes
 
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
