58f45fbfee531e1d85338352e2018246b7305d11
jwaltzjr <jwaltzjr@hotmail.com>
2017-05-01 15:31:53 -0500

    Add signup view

--- fsnd_p3_blog/blog/views.py
+++ fsnd_p3_blog/blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.template import RequestContext
 from django.views.generic import TemplateView, ListView
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import login, authenticate, views as auth_views
+from django.contrib.auth.forms import UserCreationForm
 
 from . import models
 
@@ -9,7 +11,23 @@ from . import models
 
 def main(request):
     posts = models.BlogPost.objects.all().order_by('-created')
-    return render(request, 'blog.html', context={'posts': posts})
+    context = RequestContext(request, {'post': posts})
+    return render(request, 'blog.html', context)
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('main')
+    else:
+        form = UserCreationForm()
+    context = RequestContext(request, {'form': form})
+    return render(request, 'signup.html', context)
 
 class LoginPage(auth_views.LoginView):
     template_name = 'login.html'
