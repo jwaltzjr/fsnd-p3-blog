b9a3a35863081b74dcbf1c0427153bfcf7d9690e
jwaltzjr <jwaltzjr@hotmail.com>
2017-05-01 15:53:34 -0500

    Add login url settings

--- fsnd_p3_blog/fsnd_p3_blog/settings.py
+++ fsnd_p3_blog/fsnd_p3_blog/settings.py
@@ -125,3 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Url for redirect after login
+
+LOGIN_URL = '/login'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
