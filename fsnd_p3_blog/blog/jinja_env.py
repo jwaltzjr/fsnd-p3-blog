534fb5bacbee058f403d79c9277250135dc71f80
jwaltzjr <jwaltzjr@hotmail.com>
2017-06-01 21:41:46 -0500

    Style blogpost template and add date filter to jinja env

--- fsnd_p3_blog/blog/jinja_env.py
+++ fsnd_p3_blog/blog/jinja_env.py
@@ -2,10 +2,14 @@ from jinja2 import Environment
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.urlresolvers import reverse
 
+def format_datetime(value, format='%Y-%m-%d %H:%M'):
+    return value.strftime(format)
+
 def environment(**options):
     env = Environment(**options)
     env.globals.update({
        'static': staticfiles_storage.url,
        'url': reverse,
     })
+    env.filters['format_datetime'] = format_datetime
     return env
